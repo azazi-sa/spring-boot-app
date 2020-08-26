45f878a692d9045fe87143487322ba5654b46bc9
Sunit Chatterjee <34455433+chatterjeesunit@users.noreply.github.com>
2020-08-26 09:44:26 +0530

    Added @CachePut and @CacheEvict annotations to customer service

--- src/main/java/com/dev/springdemo/customer/CustomerService.java
+++ src/main/java/com/dev/springdemo/customer/CustomerService.java
@@ -4,6 +4,8 @@ import com.dev.springdemo.customer.model.Customer;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -32,6 +34,7 @@ public class CustomerService {
         return customerRepository.findById(customerId);
     }
 
+    @CachePut(key = "#customer.id")
     public Customer create(Customer customer) {
         try {
             log.info("Creating a new customer with emailAddress: {}", customer.getEmailAddress());
@@ -43,6 +46,7 @@ public class CustomerService {
 
     }
 
+    @CachePut(key = "#customer.id")
     public Customer update(Customer customer) {
         log.info("Updating a customer with id: {}", customer.getId());
         Optional<Customer> optionalCustomer = customerRepository.findById(customer.getId());
@@ -72,6 +76,7 @@ public class CustomerService {
     }
 
 
+    @CacheEvict
     public void deleteCustomer(Long customerId) {
         try {
             customerRepository.deleteById(customerId);
