2687ab9546a5adb177a00367f2d25869a5753453
MuhammedSameer Azazi <muhammedsameer_azazi@external.mckinsey.com>
2020-09-25 18:18:39 +0530

    Add kubernetes DNS lookup cluster discovery for hazelcast

--- src/main/resources/db/migration/V5.0__AlterTable_Customer_AddUniqueIndex.sql
+++ src/main/resources/db/migration/V5.0__AlterTable_Customer_AddUniqueIndex.sql
@@ -2,4 +2,4 @@ delete from `address`;
 delete from `customer`;
 
 ALTER TABLE `customer`
-ADD UNIQUE INDEX `idx_customer_email` (`email_address` ASC) VISIBLE;
+ADD UNIQUE INDEX `idx_customer_email` (`email_address` ASC);
